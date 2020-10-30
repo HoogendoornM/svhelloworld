e84807401bc19564196d83907f812849f666fe35
MichaelH <hoog0095@hz.nl>
2020-10-30 09:53:00 +0100

    Fixed index translation not working

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,13 +3,13 @@
 
 @section('content')
     <p>
-        {{  __('Mijn SV "Hello World" is het portaal voor alle leden van Studievereniging "Hello World",
-maar ook niet-leden mogen zich aanmelden voor activiteiten via dit portaal!
-Je moet hiervoor wel student zijn aan de opleiding HBO-ICT van de HZ University of Applied Sciences.') }}
+        {{  __('Mijn SV "Hello World" is het portaal voor alle leden van Studievereniging "Hello World", maar ook niet-leden mogen zich aanmelden voor activiteiten via dit portaal! Je moet hiervoor wel student zijn aan de opleiding HBO-ICT van de HZ University of Applied Sciences.') }}
     </p>
 
     <p>
-        {{ __("Als je nieuwe ideeën hebt voor dit portaal mag je deze indienen op") }} <a href="https://github.com/sv-helloworld/mijn-sv-helloworld" target="_blank">{{ __("onze GitHub-pagina") }}</a>.
+        {{ __("Als je nieuwe ideeën hebt voor dit portaal mag je deze indienen op") }} <a
+            href="https://github.com/sv-helloworld/mijn-sv-helloworld"
+            target="_blank">{{ __("onze GitHub-pagina") }}</a>.
     </p>
 
     <p>
